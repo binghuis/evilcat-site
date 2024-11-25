b1ec1d4e5ae14cc2d59952ac3282f04a5298b536
宋秉徽 <451000936@qq.com>
2024-11-25 11:59:45 +0800

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 'use client';
-import axios from 'axios';
+import CatImg1 from '@/assets/1.jpg';
+import CatImg2 from '@/assets/2.jpg';
+import CatImg3 from '@/assets/3.jpg';
+import CatImg4 from '@/assets/4.jpg';
+import CatImg5 from '@/assets/5.jpg';
+import CatImg7 from '@/assets/7.jpg';
 import { useEffect } from 'react';
+const images = [CatImg1, CatImg2, CatImg3, CatImg4, CatImg5, CatImg7];
 
 export default function App() {
-  useEffect(() => {
-    axios.get('/api/dog?a=2');
-  }, []);
-  return (
-    <main className="">
-      {/* <ThemeSwitcher />
-      <ChatBox /> */}
-      @children
-    </main>
-  );
+  useEffect(() => {}, []);
+
+  return <main className="relative w-screen h-screen"></main>;
 }
