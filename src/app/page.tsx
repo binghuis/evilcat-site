260842b6e077369aa277c6a2c4e535a4fcd84483
宋秉徽 <451000936@qq.com>
2024-11-27 01:19:39 +0800

    feat: style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@
 import EvilCat from '@/assets/evilcat.png';
 import { Docker } from '@/components/docker';
 import ClientTweetCard from '@/components/magicui/client-tweet-card';
+import AnimatedGradientText from '@/components/ui/animated-gradient-text';
 import { BorderBeam } from '@/components/ui/border-beam';
-import { NeonGradientCard } from '@/components/ui/neon-gradient-card';
-import { VelocityScroll } from '@/components/ui/scroll-based-velocity';
-import ShinyButton from '@/components/ui/shiny-button';
 import SparklesText from '@/components/ui/sparkles-text';
 import Config from '@/config';
-import { Snippet } from '@nextui-org/react';
+import { cn, Snippet } from '@nextui-org/react';
+import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -19,63 +18,65 @@ export default function App() {
   const router = useRouter();
   useEffect(() => {}, []);
   return (
-    <main className="global-bg w-screen font-mono relative overflow-x-hidden py-32 lg:h-screen">
-      <div className="absolute left-0 top-0 w-full pt-2">
+    <main className="global-bg bg-black w-screen font-mono relative overflow-x-hidden py-32 lg:h-screen">
+      {/* <div className="absolute left-0 top-0 w-full pt-2">
         <VelocityScroll
           text="Evil Cat."
           default_velocity={2}
           className="font-display text-center text-5xl font-bold text-gray-300 tracking-[-0.02em] drop-shadow-sm lg:text-6xl sm:leading-[3.2rem]"
         />
-      </div>
-      <div className="relative z-10 h-full w-full flex flex-col justify-start items-center lg:justify-between lg:flex-row gap-8 max-w-6xl px-6 mx-auto">
-        <div className="relative shadow-2xl rounded-lg flex flex-shrink-0 flex-grow-0">
-          <ClientTweetCard id="1678577280489234432" />
-          <BorderBeam size={300} duration={12} delay={9} />
-        </div>
-        <div className="flex-1 flex flex-col gap-4 justify-start items-center sm:items-start">
-          <div className="bg-transparent backdrop-blur-xl p-2 rounded-lg shadow-lg">
+      </div> */}
+      <div className="relative z-10 h-full w-full flex flex-col justify-start items-start lg:justify-between lg:flex-row gap-8 max-w-6xl px-6 mx-auto">
+        <div className="relative shadow-2xl gap-4 rounded-lg flex flex-col flex-shrink-0 flex-grow-0">
+          <div className="bg-transparent backdrop-blur-xl p-4 rounded-xl shadow-xl">
             <SparklesText
               className="text-gray-100"
               text="$EVILCAT"
               sparklesCount={9}
               colors={{ first: '#ca8a04', second: '#ca8a04' }}
             />
+
             <div className="mt-2 hidden sm:block">
               <Snippet color="primary" variant="solid" symbol="CA:">
                 {Config.ca}
               </Snippet>
             </div>
           </div>
-
-          <div>
-            <NeonGradientCard className="z-0">
-              <span className="pointer-events-none z-10 h-full whitespace-nowrap bg-gradient-to-br from-[#ff2975] from-35% to-[#00FFF1] bg-clip-text text-center sm:text-5xl text-3xl font-bold leading-none tracking-tighter text-transparent">
-                Evil, but cute.
-              </span>
-            </NeonGradientCard>
+          <div className="bg-transparent p-4 rounded-xl backdrop-blur-lg shadow-lg pointer-events-none z-10 whitespace-nowrap ">
+            <div className="bg-gradient-to-br from-[#ff2975] from-35% to-[#00FFF1] bg-clip-text text-5xl font-bold leading-none tracking-tighter text-transparent">
+              Evil, but cute.
+            </div>
           </div>
+          <div className="relative rounded-md">
+            <ClientTweetCard id="1678577280489234432" />
+            <BorderBeam size={300} duration={12} delay={9} />
+          </div>
+        </div>
 
+        <div className="flex-1 flex flex-col gap-4 justify-start items-center sm:items-start">
           <Link
-            href={Config.url.dexscreener as unknown as UrlObject}
+            href={Config.url.raydium as unknown as UrlObject}
             target="_blank"
             className="flex justify-center sm:justify-start relative"
           >
-            <ShinyButton>
-              <div className="font-bold text-5xl sm:text-7xl text-yellow-600">
-                <span>BUY NOW</span>
-              </div>
-            </ShinyButton>
+            <AnimatedGradientText className="bg-gray-800 text-5xl rounded-full">
+              👋 <hr className="mx-2 h-6 w-px shrink-0 bg-gray-300" />{' '}
+              <span
+                className={cn(
+                  `inline animate-gradient bg-gradient-to-r font-bold from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+                )}
+              >
+                BUY NOW
+              </span>
+              <ChevronRight className="ml-1 size-6 transition-transform text-yellow-500 duration-300 ease-in-out group-hover:translate-x-1" />
+            </AnimatedGradientText>
           </Link>
         </div>
       </div>
       <div className="py-2 lg:pb-4 rounded-t-3xl fixed bottom-0 left-0 w-full z-50 bg-transparent backdrop-blur-xl sm:bg-transparent sm:backdrop-blur-none">
         <Docker />
       </div>
-      <Image
-        src={EvilCat}
-        alt=""
-        className="absolute max-w-sm md:max-w-md lg:max-w-xl -right-36 bottom-0  -z-0"
-      />
+      <Image src={EvilCat} alt="" className="absolute max-w-sm -right-24 bottom-0  -z-0" />
     </main>
   );
 }
