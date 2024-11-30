110c1fbe7aa37fd4d2fde3fe5055977773e4f3fc
宋秉徽 <451000936@qq.com>
2024-11-30 23:38:13 +0800

    feat: x img

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
+import { Spinner } from '@nextui-org/react';
+
 export default function Loading() {
-  return <>Loading...</>;
+  return (
+    <div className="flex justify-center items-start">
+      <Spinner className="mt-4" />
+    </div>
+  );
 }
